8271b140325c8a668a673dea159f21ed7657bbbb
Balasubramanian Narasimhan <b.naras@gmail.com>
2023-02-13 16:24:35 -0800

    First commit

---
+++ R/int64-package.R
@@ -0,0 +1,271 @@
+
+
+#' Class \code{"binary"}
+#' 
+#' Binary representation
+#' 
+#' 
+#' @name binary-class
+#' @aliases binary-class show,binary-method
+#' @docType class
+#' @section Objects from the Class: Objects can be created by one of the forms
+#' of the \code{\link{binary}} methods.
+#' @author Romain Francois, sponsored by the Google Open Source Programs Office
+#' @keywords classes
+#' @examples
+#' 
+#' binary( 1:4 )
+#' 
+NULL
+
+
+
+
+
+#' Get binary representation
+#' 
+#' Get binary representation
+#' 
+#' 
+#' @name binary-methods
+#' @aliases binary binary-methods binary,integer-method binary,int64-method
+#' binary,uint64-method binary,numeric-method
+#' @docType methods
+#' @section Methods: \describe{
+#' 
+#' \item{list("signature(object = \"integer\")")}{ Method for integer Vectors }
+#' 
+#' \item{list("signature(object = \"int64\")")}{ Method for
+#' \code{\linkS4class{int64}} vectors } \item{list("signature(object =
+#' \"uint64\")")}{ Method for \code{\linkS4class{uint64}} vectors }
+#' 
+#' \item{list("signature(object = \"numeric\")")}{ Method for numeric vectors }
+#' }
+#' @keywords methods
+#' @examples
+#' 
+#'     binary( 1:4 )
+#'     binary( c( 1.0, 2.0 ) )
+#'     binary( as.int64( 1:4 ) )
+#'     binary( as.uint64( 1:4 ) )
+#' 
+NULL
+
+
+
+
+
+#' Class \code{"int64"}
+#' 
+#' Vector of signed 64 bit integers
+#' 
+#' 
+#' @name int64-class
+#' @aliases int64-class [,int64-method Math,int64-method Math2,int64-method
+#' [<-,int64-method Arith,ANY,int64-method Arith,int64,ANY-method
+#' Arith,int64,int64-method as.character,int64-method names,int64-method
+#' names<-,int64-method Compare,ANY,int64-method Compare,int64,ANY-method
+#' Compare,int64,int64-method length,int64-method show,int64-method
+#' Summary,int64-method c,int64-method is.na,int64-method
+#' @docType class
+#' @section Objects from the Class: Objects can be created by using the
+#' \code{\link{int64}} function, by converting character vectors or integer
+#' vectors using the \code{\link{as.int64}} function.
+#' @author Romain Francois. Sponsored the Google Open Source Programs Office.
+#' @seealso \code{\link{as.int64}} to convert character or integer vectors.
+#' 
+#' \code{\link{int64}} to create new \code{\linkS4class{int64}} vectors of a
+#' given size.
+#' 
+#' The \code{\linkS4class{uint64}} class to represent unsigned 64 bit integer
+#' vectors.
+#' @keywords classes
+#' @examples
+#' 
+#'     x <- int64( 4 )
+#'     
+#'     # setting subsets
+#'     x[1:2] <- 1:2
+#'     x[3:4] <- c("123456789012345", "9876543219876")
+#'     x
+#'     
+#'     # arithmetic operations
+#'     x * 2L
+#'     x + x
+#'     x - 3L
+#'     
+#'     # arithmetic operations first convert both operands to 64 bit integer type
+#'     # so some precision will be lost
+#'     as.int64(1) + 1.5
+#'     # but it feels appropriate for dealing with large values
+#'     as.int64(43124567245667) + 1.5
+#'     
+#'     # logical operations
+#'     x < 3L
+#'     x != c( 1L, 2L )
+#'     
+#'     # Summary operations
+#'     range( x )
+#'     min( x )
+#'     max( x )
+#'     length(x)
+#'     
+#'     
+#'     df <- data.frame( a = 1:4 )
+#'     df$b <- x
+#'     df
+#'     
+#'     as.character( x )
+#'     
+#'     
+#' 
+NULL
+
+
+
+
+
+#' 64 bit integer types
+#' 
+#' 64 bit integer types
+#' 
+#' \tabular{ll}{ Package: \tab int64\cr Type: \tab Package\cr Version: \tab
+#' 1.1.2\cr Date: \tab 2011-12-01\cr License: \tab GPL (>= 2)\cr LazyLoad: \tab
+#' yes\cr }
+#' 
+#' @name int64-package
+#' @docType package
+#' @author Romain Francois, Sponsored by Google Open Source Programs Office
+#' 
+#' Maintainer: Romain Francois <romain@@r-enthusiasts.com>
+#' @keywords package
+#' @examples
+#' 
+#'     as.int64( 1:4 )
+#'     as.int64( c("123456789", "9876543219876543" ) )
+#' 
+NULL
+
+
+
+
+
+#' Sorting 64 bits integer vector
+#' 
+#' Sorting 64 bits integer vector
+#' 
+#' 
+#' @name sort-methods
+#' @aliases sort-methods sort,ANY-method sort,int64-method sort,uint64-method
+#' @docType methods
+#' @section Methods: \describe{
+#' 
+#' \item{list("signature(x = \"ANY\")")}{ Standard method (from base) }
+#' 
+#' \item{list("signature(x = \"int64\")")}{ Sorting signed 64 bit integer
+#' vectors (\code{\linkS4class{int64}} }
+#' 
+#' \item{list("signature(x = \"uint64\")")}{ Sorting unsigned 64 bit integer
+#' vectors (\code{\linkS4class{uint64}} } }
+#' @keywords methods
+NULL
+
+
+
+
+
+#' Class \code{"uint64"}
+#' 
+#' Vector of signed 64 bit integers
+#' 
+#' 
+#' @name uint64-class
+#' @aliases uint64-class Math,uint64-method Math2,uint64-method [,uint64-method
+#' [<-,uint64-method Arith,ANY,uint64-method Arith,uint64,ANY-method
+#' Arith,uint64,uint64-method as.character,uint64-method names,uint64-method
+#' names<-,uint64-method Compare,ANY,uint64-method Compare,uint64,ANY-method
+#' Compare,uint64,uint64-method length,uint64-method show,uint64-method
+#' Summary,uint64-method c,uint64-method is.na,uint64-method
+#' @docType class
+#' @section Objects from the Class: Objects can be created by using the
+#' \code{\link{uint64}} function, by converting character vectors or integer
+#' vectors using the \code{\link{as.uint64}} function.
+#' @author Romain Francois. Sponsored the Google Open Source Programs Office.
+#' @seealso \code{\link{as.uint64}} to convert character or integer vectors.
+#' 
+#' \code{\link{uint64}} to create new \code{\linkS4class{uint64}} vectors of a
+#' given size.
+#' 
+#' The \code{\linkS4class{int64}} class to represent signed 64 bit integer
+#' vectors.
+#' @keywords classes
+#' @examples
+#' 
+#'     x <- uint64( 4 )
+#'     
+#'     # setting subsets
+#'     x[1:2] <- 1:2
+#'     x[3:4] <- c("123456789012345", "9876543219876")
+#'     x
+#'     
+#'     # arithmetic operations
+#'     x * 2L
+#'     x + x
+#'     x - 3L
+#'     
+#'     # logical operations
+#'     x < 3L
+#'     x != c( 1L, 2L )
+#'     
+#'     # Summary operations
+#'     range( x )
+#'     min( x )
+#'     max( x )
+#'     length(x)
+#'     
+#'     
+#'     df <- data.frame( a = 1:4 )
+#'     df$b <- x
+#'     df
+#'     
+#'     as.character( x )
+#'     
+#'     
+#' 
+NULL
+
+
+
+
+
+#' Unique implementation for 64 bit integer vectors
+#' 
+#' Implementation of \code{\link{unique}} for 64 bit integer vectors
+#' 
+#' 
+#' @name unique-methods
+#' @aliases unique-methods unique,ANY-method unique,int64-method
+#' unique,uint64-method
+#' @docType methods
+#' @section Methods: \describe{
+#' 
+#' \item{list("signature(x = \"ANY\")")}{default implementation (from base)}
+#' 
+#' \item{list("signature(x = \"int64\")")}{ signed 64 bit integer vectors.
+#' \code{\linkS4class{int64}} }
+#' 
+#' \item{list("signature(x = \"uint64\")")}{ unsigned 64 bit integer vectors.
+#' \code{\linkS4class{uint64}} } }
+#' @keywords methods
+#' @examples
+#' 
+#'     x <- as.int64( c(1:5, 1L, 5L) )
+#'     unique( x )
+#' 
+#'     x <- as.uint64( c(1:5, 1L, 5L) )
+#'     unique( x )    
+#' 
+NULL
+
+
+
---
+++ R/int64.R
@@ -0,0 +1,436 @@
+# Copyright (C) 2011 Romain Francois
+# Copyright (C) 2011 Google Inc.  All rights reserved.
+#
+# This file is part of int64.
+#
+# int64 is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# int64 is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with int64.  If not, see <http://www.gnu.org/licenses/>.
+
+setClassUnion( "maybeNames", c("character", "NULL" ) )
+
+names_int64 <- function(x){
+    x@NAMES    
+}
+namesgets_int64 <- function( x, value){
+    if( missing(value) || is.null(value) ){
+        x@NAMES <- NULL
+    } else if( is.character( value )){
+        if( length( value ) == length( x@.Data ) ){
+            x@NAMES <- value
+        } else if(length(value) < length(x@.Data) ) {
+            x@NAMES <- c( value, rep( NA, length(x@.Data) - length(value) ) )
+        } else {
+            stop( "error assigning names" )
+        }
+    } else {
+        stop( "must be character vector or NULL" )
+    }
+    x    
+}
+setClass( "int64", contains = "list", 
+    representation( NAMES = "maybeNames")
+)
+setClass( "uint64", contains = "list" ,
+    representation( NAMES = "maybeNames")
+)
+
+setMethod( "names", "int64", names_int64 )
+setMethod( "names<-", "int64", namesgets_int64 )
+setMethod( "names", "uint64", names_int64 )
+setMethod( "names<-", "uint64", namesgets_int64 )
+
+
+setClass( "binary",  
+    representation( data = "character", bits = "integer" )
+)
+
+setGeneric( "binary", function(object) standardGeneric("binary") )
+setMethod( "binary", "integer", function(object){
+   new( "binary", data = .Call( int64_format_binary, object ), bits = 32L ) 
+} )
+setMethod( "binary", "numeric", function(object){
+   new( "binary", data = .Call( int64_format_binary, object ), bits = 64L ) 
+} )
+setMethod( "binary", "int64", function(object){
+   new( "binary", data = .Call( int64_format_binary, object ), bits = 64L ) 
+} )
+setMethod( "binary", "uint64", function(object){
+   new( "binary", data = .Call( int64_format_binary, object ), bits = 64L ) 
+} )
+setMethod( "show", "binary", function(object){
+    print( noquote( object@data ) )
+    invisible(object)
+})
+
+
+
+#' Creates new int64 vectors of a given length
+#' 
+#' Creates new \code{\linkS4class{int64}} vectors of a given length
+#' 
+#' 
+#' @param length Length of the resulting vector
+#' @return A new \code{\linkS4class{int64}} vector of the given length
+#' @author Romain Francois, Sponsored by the Google Open Source Programs Office
+#' @seealso \code{\link{as.int64}} for converting integer or character vectors
+#' into \code{\linkS4class{int64}} vectors.
+#' 
+#' \code{\link{uint64}} for unsigned 64 bit integer vectors, i.e. of class
+#' \code{\linkS4class{uint64}}
+#' @keywords manip
+#' @examples
+#' 
+#' x <- int64(10L)
+#' 
+#' @export int64
+int64 <- function(length=0L){
+    x <- new("int64", rep( list(integer(2L)), length ) )
+    x
+}
+
+
+#' Creates new uint64 vectors of a given length
+#' 
+#' Creates new \code{\linkS4class{uint64}} vectors of a given length
+#' 
+#' 
+#' @param length Length of the resulting vector
+#' @return A new \code{\linkS4class{uint64}} vector of the given length
+#' @author Romain Francois, Sponsored by the Google Open Source Programs Office
+#' @seealso \code{\link{as.uint64}} for converting integer or character vectors
+#' into \code{\linkS4class{uint64}} vectors.
+#' 
+#' \code{\link{int64}} for signed 64 bit integer vectors, i.e. of class
+#' \code{\linkS4class{int64}}.
+#' @keywords manip
+#' @examples
+#' 
+#' x <- uint64(10L)
+#' 
+#' @export uint64
+uint64 <- function(length=0L){
+    x <- new("uint64", rep( list(integer(2L)), length ) )
+    x
+}
+
+setMethod( "length", "int64", function(x){
+    length(x@.Data)
+} )
+setMethod( "length", "uint64", function(x){
+    length(x@.Data)
+} )
+show_int64 <- function(object){
+    if( !length(object)) {
+        writeLines( sprintf( "%s(0)", class(object) ) )
+    } else { 
+        if( is.null( object@NAMES ) ){
+            print( noquote( as.character( object ) ) )
+        } else {
+            x <- as.character( object )
+            names(x) <- object@NAMES
+            print(noquote(x))
+        }
+    }
+    invisible(object)
+} 
+setMethod( "show", "int64", show_int64)
+setMethod( "show", "uint64", show_int64)
+
+
+
+#' Convert character or integer vectors into int64 vectors.
+#' 
+#' Convert character or integer vectors into \code{\linkS4class{int64}}
+#' vectors.
+#' 
+#' For conversion of character vectors, the C function \code{atol} is used.
+#' 
+#' @param x A character or integer vector
+#' @return A new \code{\linkS4class{int64}} vector.
+#' @author Romain Francois, sponsored by the Google Open Source Programs Office
+#' @seealso \code{\link{as.uint64}} for conversion to unsigned long vectors.
+#' @references C++ \code{atol} function:
+#' \url{http://www.cplusplus.com/reference/clibrary/cstdlib/atol/}
+#' @keywords manip
+#' @examples
+#' 
+#' as.int64( c(1L, 2L ) )
+#' as.int64( c("123456789123456", "987654321987654" ) )
+#' 
+#' @export as.int64
+as.int64 <- function(x){
+    if( is.character(x) ){
+        wrong <- ! grepl("^[-]?[0-9]+$", x) 
+        if( any(wrong) ){
+            x[wrong] <- "NA"
+            warning( "NAs introduced" )
+        }
+    }
+    new( "int64", .Call(int64_as_int64, x) ) 
+}
+
+
+#' Convert character or integer vectors into uint64 vectors.
+#' 
+#' Convert character or integer vectors into \code{\linkS4class{uint64}}
+#' vectors.
+#' 
+#' For conversion of character vectors, the C function \code{atol} is used.
+#' 
+#' @param x A character or integer vector
+#' @return A new \code{\linkS4class{int64}} vector.
+#' @author Romain Francois, sponsored by the Google Open Source Programs Office
+#' @seealso \code{\link{as.int64}} for conversion to signed long vectors.
+#' @references C++ \code{atol} function:
+#' \url{http://www.cplusplus.com/reference/clibrary/cstdlib/atol/}
+#' @keywords manip
+#' @examples
+#' 
+#' as.uint64( c(1L, 2L ) )
+#' as.uint64( c("123456789123456", "987654321987654" ) )
+#' 
+#' @export as.uint64
+as.uint64 <- function(x){
+    if( is.character(x) ){
+        wrong <- ! grepl("^[0-9]+$", x) 
+        if( any(wrong) ){
+            x[wrong] <- "NA"
+            warning( "NAs introduced" )
+        }
+    }
+    new( "uint64", .Call(int64_as_uint64, x) ) 
+}
+
+setMethod( "[", "int64", function(x, i, j, ...){
+    new( "int64", x@.Data[ i ] )
+} )
+setMethod( "[", "uint64", function(x, i, j, ...){
+    new( "uint64", x@.Data[ i ] )
+} )
+setMethod( "[<-", "int64", function(x, i, j, ..., value ){
+    data <- x@.Data
+    data[i] <- as.int64( value )@.Data
+    new( "int64", data )
+} )
+setMethod( "[<-", "uint64", function(x, i, j, ..., value ){
+    data <- x@.Data
+    data[i] <- as.uint64( value )@.Data
+    new( "uint64", data )
+} )
+
+
+
+setMethod( "Arith", signature(e1 = "int64", e2 = "int64" ), 
+function(e1,e2){
+   numbers <- .Call( int64_arith_int64_int64, .Generic, e1, e2, FALSE )
+   new( "int64", numbers ) 
+} )
+setMethod( "Arith", signature(e1 = "int64", e2 = "ANY" ), 
+function(e1,e2){
+   numbers <- .Call( int64_arith_int64_int64, .Generic, e1, as.int64(e2), FALSE )
+   new( "int64", numbers )     
+} )
+setMethod( "Arith", signature(e1 = "ANY", e2 = "int64" ), 
+function(e1,e2){
+   numbers <- .Call( int64_arith_int64_int64, .Generic, as.int64(e1), e2, FALSE )
+   new( "int64", numbers ) 
+} )
+
+setMethod( "Arith", signature(e1 = "uint64", e2 = "uint64" ), 
+function(e1,e2){
+   numbers <- .Call( int64_arith_int64_int64, .Generic, e1, e2, TRUE )
+   new( "uint64", numbers ) 
+} )
+setMethod( "Arith", signature(e1 = "uint64", e2 = "ANY" ), 
+function(e1,e2){
+   numbers <- .Call( int64_arith_int64_int64, .Generic, e1, as.uint64(e2), TRUE )
+   new( "uint64", numbers )     
+} )
+setMethod( "Arith", signature(e1 = "ANY", e2 = "uint64" ), 
+function(e1,e2){
+   numbers <- .Call( int64_arith_int64_int64, .Generic, as.uint64(e1), e2, TRUE )
+   new( "uint64", numbers ) 
+} )
+
+setMethod( "Compare", signature(e1 = "int64", e2 = "int64" ), 
+function(e1,e2){
+   .Call( int64_compare_int64_int64, .Generic, e1, e2, FALSE )
+} )
+setMethod( "Compare", signature(e1 = "ANY", e2 = "int64" ), 
+function(e1,e2){
+   .Call( int64_compare_int64_int64, .Generic, as.int64(e1), e2, FALSE )
+} )
+setMethod( "Compare", signature(e1 = "int64", e2 = "ANY" ), 
+function(e1,e2){
+   .Call( int64_compare_int64_int64, .Generic, e1, as.int64(e2), FALSE )
+} )
+
+setMethod( "Compare", signature(e1 = "uint64", e2 = "uint64" ), 
+function(e1,e2){
+   .Call( int64_compare_int64_int64, .Generic, e1, e2, TRUE )
+} )
+setMethod( "Compare", signature(e1 = "ANY", e2 = "uint64" ), 
+function(e1,e2){
+   .Call( int64_compare_int64_int64, .Generic, as.uint64(e1), e2, TRUE )
+} )
+setMethod( "Compare", signature(e1 = "uint64", e2 = "ANY" ), 
+function(e1,e2){
+   .Call( int64_compare_int64_int64, .Generic, e1, as.uint64(e2), TRUE)
+} )
+
+
+setMethod( "Summary", "int64", function(x,..., na.rm = FALSE){
+   .Call( int64_summary_int64, .Generic, x, FALSE)
+} )
+setMethod( "Summary", "uint64", function(x,..., na.rm = FALSE){
+   .Call( int64_summary_int64, .Generic, x, TRUE)
+} )
+
+
+setMethod( "as.character", "int64", function(x,...){
+  .Call( int64_as_character_int64, x, FALSE)  
+})
+setMethod( "as.character", "uint64", function(x,...){
+  .Call( int64_as_character_int64, x, TRUE)  
+})
+as.data.frame.int64 <- as.data.frame.uint64 <- as.data.frame.vector
+format.int64 <- format.uint64 <- function(x, ...){
+    as.character(x)   
+}
+
+
+
+#' Give numeric limits of integer types
+#' 
+#' Give numeric limits of integer types
+#' 
+#' 
+#' @param type type. must be integer, int64 or uint64.
+#' @return A vector of two values of the appropriate type.
+#' @author Romain Francois, sponsored by the Google Open Source Programs Office
+#' @keywords manip
+#' @examples
+#' 
+#' numeric_limits( "integer" )
+#' numeric_limits( "int64" )
+#' numeric_limits( "uint64" )
+#' 
+#' @export numeric_limits
+numeric_limits <- function( type ){
+    .Call( int64_limits, type )
+}
+
+setGeneric( "unique" )
+setMethod( "unique", "int64", function(x, incomparables = FALSE, ...){
+    new( "int64", .Data = unique( x@.Data, incomparables, ... ) )  
+} )
+setMethod( "unique", "uint64", function(x, incomparables = FALSE, ...){
+    new( "uint64", .Data = unique( x@.Data, incomparables, ... ) )  
+} )
+
+setGeneric( "sort" )
+setMethod( "sort", "int64", function(x, decreasing = FALSE, ...){
+    .Call( int64_sort, x, FALSE, decreasing )
+} )
+setMethod( "sort", "uint64", function(x, decreasing = FALSE, ...){
+    .Call( int64_sort, x, TRUE, decreasing )
+} )
+
+setMethod( "Math", "int64", function(x){
+    .Call( int64_math, .Generic, x, FALSE)
+} )
+setMethod( "Math", "uint64", function(x){
+    .Call( int64_math, .Generic, x, TRUE )
+} )
+
+# implementation of signif using string maniplation
+int64_Math2 <- function( type = "int64", .Generic, x, digits ){
+    if( .Generic == "round" ) x else{
+        if( any(digits<0 ) ) stop("digits must be positive")
+        
+        # signif
+        s <- as.character( x )
+        len <- nchar( s )
+        signs <- ! grepl( "^-", s )
+        s <- sub( "^-", "", s ) 
+        
+        # recycling
+        digits <- as.integer( rep( digits, length = length( s ) ) )
+        digits[ digits == 0L ] <- 1L
+        
+        res <- .Call( int64_signif, s, digits, len )
+        res <- sprintf( "%s%s", ifelse(signs, "", "-"), res )
+        
+        if( type == "int64" ) as.int64(res) else as.uint64(res) 
+    }
+}
+
+setMethod( "Math2", "int64", function(x, digits  = 6L){
+    int64_Math2( "int64", .Generic, x, digits )
+} )
+setMethod( "Math2", "uint64", function(x, digits = 6L){
+    int64_Math2( "uint64", .Generic, x, digits )
+} )
+
+
+setMethod( "is.na", "int64", function(x){
+  .Call( int64_isna, x, FALSE )  
+})
+setMethod( "is.na", "uint64", function(x){
+  .Call( int64_isna, x, TRUE )  
+})
+
+c_int64 <- function(as, ctor){
+    function(x, ..., recursive = FALSE ){
+    dots <- list(...)
+    if( !length(dots) ) return(x)
+    
+    dots <- lapply( dots, function(x) as(x)@.Data )
+    n <- length(x) + sum( sapply( dots, length ) ) 
+    
+    res <- ctor(n) 
+    
+    res@.Data[ 1:length(x) ] <- x@.Data
+    start <- length(x)+1L
+    for( i in 1:length(dots)){
+        data <- dots[[i]]
+        res@.Data[ start:(start+length(data)-1L) ] <- data
+        start <- start + length(data)
+    }
+    res
+} 
+}
+
+
+setMethod( "c", "int64", c_int64( as.int64, int64 ) )
+setMethod( "c", "uint64", c_int64( as.uint64, uint64 ) )
+
+setAs("character", "int64", function(from) as.int64(from))
+setAs("character", "uint64", function(from) as.uint64(from))
+
+setAs("integer", "int64", function(from) as.int64(from))
+setAs("integer", "uint64", function(from) as.uint64(from))
+
+setAs("logical", "int64", function(from) as.int64(from))
+setAs("logical", "uint64", function(from) as.uint64(from))
+
+setAs("numeric", "int64", function(from) as.int64(from))
+setAs("numeric", "uint64", function(from) as.uint64(from))
+
+str.int64 <- str.uint64 <- function(object, ...){
+    writeLines( sprintf( " %s [1:%d] %s ...", class(object), length(object), 
+        paste( as.character( head( object, 3 ) ), collapse = " " )
+    ) )
+}
+
+
